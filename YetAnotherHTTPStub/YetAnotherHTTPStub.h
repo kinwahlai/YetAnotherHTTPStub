03dfe6ae0baee7b5fb83dce8ca709991be4822c1
Darren Lai <dlai@stelladot.com>
2017-07-07 20:56:04 -0700

    Initial Commit

---
+++ YetAnotherHTTPStub/YetAnotherHTTPStub.h
@@ -0,0 +1,19 @@
+//
+//  YetAnotherHTTPStub.h
+//  YetAnotherHTTPStub
+//
+//  Created by Darren Lai on 7/7/17.
+//  Copyright © 2017 KinWahLai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YetAnotherHTTPStub.
+FOUNDATION_EXPORT double YetAnotherHTTPStubVersionNumber;
+
+//! Project version string for YetAnotherHTTPStub.
+FOUNDATION_EXPORT const unsigned char YetAnotherHTTPStubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YetAnotherHTTPStub/PublicHeader.h>
+
+
